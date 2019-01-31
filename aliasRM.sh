40a504d952658ccc7ec3d5a53158166379517c73
wqc0217 <wqc0217@126.com>
2019-01-31 10:25:26 +0800

    Create aliasRM.sh

---
+++ aliasRM.sh
@@ -0,0 +1,26 @@
+mkdir -p ~/.trash
+alias rm=trash
+alias r=trash
+alias rl='ls ~/.trash'
+alias ur=undelfile
+
+undelfile()
+{
+	if [ ! -d "~/.trash" ]; then
+		mkdir -p ~/.trash
+	fi
+    mv -i  ~/.trash/$@ ./
+}
+
+trash()
+{
+	
+    mv $@ ~/.trash/
+}
+
+cleartrash()
+{
+   read -p "clear sure?[n]" confirm
+   [ $confirm == 'y' ] || [ $confirm == 'Y' ]  && /usr/bin/rm -rf ~/.trash/*
+}
+
